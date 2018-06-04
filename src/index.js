7f0138310e192f022acdf916b1ae67ad2097499e
Aris Julio <aj.julio@hotmail.com>
2018-06-03 19:00:06 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.css';
+import Home from './components/Home';
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('root')
+);
